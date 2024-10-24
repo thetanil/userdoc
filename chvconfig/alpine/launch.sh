045b01788241a975d37645da4b4692842d62408f
Theta Nil <20186755+thetanil@users.noreply.github.com>
2024-10-25 01:57:55 +0200

    tty pls

--- chvconfig/alpine/launch.sh
+++ chvconfig/alpine/launch.sh
@@ -5,21 +5,32 @@ set -e
 # --initramfs ./initramfs-virt \
 # cloud-hypervisor \
 #     --kernel ./vmlinuz-virt \
-# 	--disk path=../../images/alpine-uefi-2024-10-24.qcow2 \
-# 	--cmdline "console=hvc0 root=/dev/vda1 init=/sbin/poweroff -f" \
-# 	--cpus boot=4 \
-# 	--memory size=1G
+#     --initramfs ./initramfs-virt \
+#     --disk path=../../images/alpine-bios-2024-10-24.qcow2 \
+#     --cmdline "console=hvc0 root=/dev/vda1 init=/bin/sh" \
+#     --cpus boot=4 \
+#     --memory size=1G
 
 # sudo apt install qemu-kvm libvirt-daemon-system libvirt-clients bridge-utils virt-manager
 # sudo systemctl enable libvirtd
 # sudo systemctl start libvirtd
 # sudo systemctl enable --now libvirtd	
 
-
     # -drive file=../../images/alpine-uefi-2024-10-24.qcow2,if=virtio \
+    # -initrd ./initramfs-virt \
+    # -device virtio-net,netdev=net0 \
+    # -netdev user,id=net0,hostfwd=tcp::2222-:22 \
+    # -device virtio-blk-pci,drive=vd0 \
+    # -kernel ./vmlinuz-virt \
+    # -initrd ./initramfs-virt \
+    # -append "console=ttyS0 root=/dev/vda init=/bin/sh" \
+    # -serial mon:stdio \
+    # -d guest_errors,unimp
+    # -smp 4 \
 qemu-system-x86_64 \
-    -kernel ./vmlinuz-virt \
-	-initrd ./initramfs-virt \
-    -append "console=ttyS0 root=/dev/vda1 init=/bin/sh" \
-    -smp 4 \
-    -m 1G
+    -drive file=../../images/alpine-bios-2024-10-24.qcow2,if=virtio \
+    -m 1G \
+    -nographic \
+    -cpu host \
+    -serial mon:stdio \
+    -enable-kvm 
