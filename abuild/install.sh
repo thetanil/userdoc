6878a91e0911944a0c1d3530b6b4a7654c392502
Theta Nil <20186755+thetanil@users.noreply.github.com>
2024-10-25 23:39:19 +0200

    nographic on install dev env

--- abuild/install.sh
+++ abuild/install.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 set -e
 
+# -display gtk \
+
 qemu-system-x86_64 \
     -m 512 \
     -cpu host \
     -enable-kvm \
+    -nographic \
     -nic user \
     -boot d \
     -cdrom alpine-standard-3.20.3-x86_64.iso \
-    -hda alpine.qcow2 \
-    -display gtk \
-    -enable-kvm
+    -hda alpine.qcow2
