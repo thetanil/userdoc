2701ebdbe79347bbce54c1fe8f5142571cb7e347
Theta Nil <20186755+thetanil@users.noreply.github.com>
2024-11-01 20:16:25 +0100

    casts 0x00 to unsigned char, ensuring it fits into the 8-bit %al register

--- abuild/qemu-kernel-build/src/dumb-exit/dumb-exit.c
+++ abuild/qemu-kernel-build/src/dumb-exit/dumb-exit.c
@@ -2,6 +2,6 @@
 #include <stdlib.h>
 
 int main() {
-    asm("outb %0, %1" : : "a"(0x00), "Nd"(0x501));  // Replace 0x00 with an exit code if needed
+    asm("outb %0, %1" : : "a"((unsigned char)0x00), "Nd"(0x501));  // Replace 0x00 with an exit code if needed
     return 0;
 }
