11e52badaa4e97a22c77127fe50e23ee9ff2cf7c
Theta Nil <20186755+thetanil@users.noreply.github.com>
2024-11-02 21:38:02 +0100

    qrun change

--- carlvm/zip/qrun.sh
+++ carlvm/zip/qrun.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 set -e
 
-echo qrun ran
+qemu-system-x86_64 \
+          -kernel ./bzImage \
+          -initrd ./alpine-minirootfs-3.20.3-x86_64.cpio \
+          -m 64M \
+          -cpu host \
+          -no-reboot \
+          -device isa-debug-exit \
+          -enable-kvm  \
+          -nographic
--- launch.sh
+++ launch.sh
@@ -65,3 +65,13 @@ qemu-system-x86_64 \
           -boot d \
           -enable-kvm  \
           -nographic
+
+qemu-system-x86_64 \
+          -kernel ./bzImage \
+          -initrd ./alpine-minirootfs-3.20.3-x86_64.cpio \
+          -m 64M \
+          -cpu host \
+          -no-reboot \
+          -device isa-debug-exit \
+          -enable-kvm  \
+          -nographic
