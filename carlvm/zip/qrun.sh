67412f8082ed680518e315aea3c684b1aaf7a3bf
Theta Nil <20186755+thetanil@users.noreply.github.com>
2024-11-03 22:00:41 +0100

    notsc

--- carlvm/zip/qrun.sh
+++ carlvm/zip/qrun.sh
@@ -3,6 +3,7 @@
 qemu-system-x86_64 \
           -kernel ./bzImage \
           -initrd ./alpine-minirootfs-3.20.3-x86_64.cpio \
+          -append "notsc" \
           -m 64M \
           -cpu host \
           -no-reboot \
