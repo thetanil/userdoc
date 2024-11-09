e3e2b3a6fc080e9115339787be8ce020f5d78740
Theta Nil <20186755+thetanil@users.noreply.github.com>
2024-11-09 17:32:15 +0100

    mount share with carlvm

--- carlvm-build/qrun.sh
+++ carlvm-build/qrun.sh
@@ -1,31 +1,15 @@
 #!/bin/bash
 
-# qemu-system-x86_64 \
-#           -kernel ./bzImage \
-#           -initrd ./alpine-minirootfs-3.20.3-x86_64.cpio \
-#           -append "notsc" \
-#           -m 64M \
-#           -cpu host \
-#           -no-reboot \
-#           -device isa-debug-exit \
-#           -enable-kvm  \
-#           -nographic
-    # -drive file=mydisk.qcow2,format=qcow2 \
-    # -hda mydisk.qcow2 \
-    # -drive format=raw,file=myimage.img \
-    # -fsdev local,security_model=passthrough,id=fsdev0,path=/tmp/share \
-    # -device virtio-9p-pci,id=fs0,fsdev=fsdev0,mount_tag=hostshare
-
 qemu-system-x86_64 \
-    -kernel ./bzImage \
-    -initrd ./alpine-minirootfs-3.20.3-x86_64.cpio \
-    -append "notsc" \
-    -m 512 \
+    -display none \
+    -no-user-config \
+    -nodefaults \
+    -m 64M \
+    -serial stdio \
     -cpu host \
-    -no-reboot \
-    -device isa-debug-exit \
     -enable-kvm  \
-    -append "root=/dev/vda console=ttyS0 nogui" \
-    -nographic \
+    -kernel ./bzImage \
+    -append "notsc" \
+    -initrd ./alpine-minirootfs-3.20.3-x86_64.cpio \
     -drive file=mydisk.qcow2,media=disk,if=virtio,cache=writeback \
-    -virtfs local,path=./shared,mount_tag=hostshare,security_model=passthrough,id=hostshare
+    -virtfs local,path=./shared,mount_tag=host0,security_model=none,id=host0
